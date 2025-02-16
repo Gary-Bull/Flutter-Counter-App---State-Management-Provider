5a204762d32d633c2a5d5e2ade8e3401876cfad3
Gary-Bull <garydena12@gmail.com>
2025-02-16 14:16:52 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/state_mgmt_provider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.state_mgmt_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_mgmt_provider/counter_model.dart';
+import 'package:state_mgmt_provider/home_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
